1c456ffd5aae5d68ebbeb099d5ff0d23ec5fa7b2
ElNgatia <ngatiaelvisw@gmail.com>
2023-07-18 13:35:53 +0300

    Fix update app name

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
